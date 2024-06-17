df42f7f91e9129e9714ea15eb20f9258b7f35bbb
IMTIAZ AHAMED RAFI <145882663+imtiazahamedrafi@users.noreply.github.com>
2024-06-17 14:36:01 +0600

    Add files via upload

---
+++ Even or odd calculator (1).c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("***********************************\n*PROGRAM FOR CALCULATE EVEN OR ODD*\n*******By IMTIAZ AHAMED RAFI*******\n***********************************");    
+    int a;
+    printf("\n ENTER THE NUMBER:");
+    scanf("%d",&a);
+    if (a%2==0) { printf("THE NUMBER %d IS EVEN \n Service end",a); 
+    }
+    else { 
+    printf("THE NUMBER %d IS ODD \n Service end",a); 
+    }
+    
+    return 0;
+    }
---
+++ SERIES CALCULATOR.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{ int a, b, c, sum;
+printf("***********************\n*NAMATA NIRNOY PROGRAM*\n*By IMTIAZ AHAMED RAFI*\n***********************\n");
+printf("\nEnter the Series number:");scanf("\n%d",&b);
+printf("\nEnter how much you want:");scanf("\n%d",&c);
+for ( a = 1; a<=c; a++) { printf("\n%dx%d = %d",b, a , b*a); }
+printf("\n\n\nservice end successfully :)");
+    return 0; }
